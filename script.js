09a889bd2b34afbaf18bdb411f3eb0ad3d89a8d5
Md Sarfraj <116196482+mdsarfarj@users.noreply.github.com>
2023-07-23 13:40:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-function volume_sphere() {
-    //Write your code here
-   const radiusInput = document.getElementById("radius");
+function volume_sphere(event) {
+    event.preventDefault(); // Prevent form submission and page reload
+
+    // Get the radius value from the input field
+    const radiusInput = document.getElementById("radius");
     const radius = parseFloat(radiusInput.value);
 
     // Check if the radius is a valid number
     if (isNaN(radius) || radius <= 0) {
         alert("Please enter a valid positive number for the radius.");
-        return false; // Returning false prevents the form from submitting
+        return;
     }
 
     // Calculate the volume of the sphere
@@ -15,8 +17,10 @@ function volume_sphere() {
     // Update the "Volume" field with the calculated volume
     const volumeField = document.getElementById("volume");
     volumeField.value = volume.toFixed(2);
+}
 
-    return false;
-} 
-
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+// Attach the volume_sphere() function to the form's onsubmit event
+window.onload = function() {
+    const form = document.getElementById('MyForm');
+    form.onsubmit = volume_sphere;
+};
